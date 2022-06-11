f9cf8387c6f1f827ee90b04f01d539da7b20d475
binanStar <williamleo0906@gmail.com>
2022-06-12 03:10:40 +0600

    Routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import './App.scss';
+import "./App.scss";
+import TodoList from "./components/TodoList/TodoIndex";
+import Login from "./components/Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<TodoList />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
